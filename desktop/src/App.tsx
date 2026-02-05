8d28ca132371bddc3ba67eabc25089a5c89924b8
dabby04 <97193496+dabby04@users.noreply.github.com>
2026-02-04 19:30:36 -0800

    Setup react+ electron

---
+++ desktop/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/electron-vite.animate.svg'
+import './App.css'
+
+function App() {
+  return <h1>Hello from Desktop 👋</h1>
+}
+
+export default App

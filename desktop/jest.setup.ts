33927d0acb1426a37aa83a082557bea3706480c0
Shreya Saxena <shreya.saxena@gmail.com>
2026-02-09 22:38:34 -0800

    Updated files to have passing tests along with package.json.

--- desktop/jest.setup.ts
+++ desktop/jest.setup.ts
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom';
+
+// Polyfill for TextEncoder/TextDecoder required by react-router-dom
+if (typeof global.TextEncoder === 'undefined') {
+  const { TextEncoder, TextDecoder } = require('util');
+  global.TextEncoder = TextEncoder;
+  global.TextDecoder = TextDecoder;
+}

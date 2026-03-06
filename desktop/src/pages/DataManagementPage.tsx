0201e9cc5f732402003ee8c35faad615a0fa2b70
Shreya Saxena <shreya.saxena@gmail.com>
2026-03-06 01:58:34 -0800

    Created data management page for chronological corrections/additions, can also be used for reranking if needed.

---
+++ desktop/src/pages/DataManagementPage.tsx
@@ -0,0 +1,18 @@
+import "../styles/DataManagementPage.css";
+
+/**
+ * Data Management Page - View and edit chronological information
+ * for projects and skills uploaded to the app.
+ */
+export function DataManagementPage() {
+  return (
+    <div className="data-management-container">
+      <h1 className="data-management-title">Data Management</h1>
+      <p className="data-management-description">
+        View and edit chronological information for all projects and skills in your app.
+      </p>
+    </div>
+  );
+}
+
+export default DataManagementPage;
